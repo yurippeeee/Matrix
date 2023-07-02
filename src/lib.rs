bbaf7f05e558a552f1b15131220a09ed7424d101
yurippeeee <isshi0929@icloud.com>
2023-07-02 15:37:26 +0900

    [add] 行列作成関数、配列取得関数作成

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,31 @@ pub mod NdMatrix{
 
     #[derive(Debug)]
     //  行列計算のベースとなる型
-    pub struct NDMATRIX<T, const N: usize>(pub [T; N]) where T:Add+Copy;
+    pub struct NDMATRIX<T, const N: usize>([T; N]) where T:Add+Copy;
+
+    impl<T, const N: usize> NDMATRIX<T,N>
+        where T:Add + Copy{
+            // 新規行列作成
+            pub fn new(m:&[T; N]) -> Self{
+                Self(*m)
+            }
+            // 配列を取得する
+            pub fn to_array(self) -> [T; N]{
+                self.0
+            }
+        }
 
     // ＋演算子実装
-    impl<T, const N: usize> Add for NDMATRIX<T,N> where T:Add<Output = T> + Copy{
-        type Output = Self;
-        fn add(self, other:Self) -> Self::Output{
-            let mut output = self.0;
-            for i in 0..self.0.len(){
-                output[i] = self.0[i] + other.0[i];
+    impl<T, const N: usize> Add for NDMATRIX<T,N> 
+        where T:Add<Output = T> + Copy{
+            type Output = Self;
+            fn add(self, other:Self) -> Self::Output{
+                let mut output = self.0;
+                for i in 0..self.0.len(){
+                    output[i] = self.0[i] + other.0[i];
+                }
+                NDMATRIX(output)
             }
-            NDMATRIX(output)
         }
-    }
 }
 
--- tests/test.rs
+++ tests/test.rs
@@ -4,7 +4,7 @@ use Matrix::NdMatrix;
 // 加算演算子のテスト
 #[test]
 fn test_add_operator() {
-    let test = NdMatrix::NDMATRIX([3,3,3,3,3]);
-    let test2 = NdMatrix::NDMATRIX([5,5,5,5,5]);
-    assert!((test+test2).0.iter().zip([8,8,8,8,8].iter()).all(|(a,b)| a == b), "Arrays are not equal");
+    let test = NdMatrix::NDMATRIX::new(&[3,3,3,3,3]);
+    let test2 = NdMatrix::NDMATRIX::new(&[5,5,5,5,5]);
+    assert!((test+test2).to_array().iter().zip([8,8,8,8,8].iter()).all(|(a,b)| a == b), "Arrays are not equal");
 }
